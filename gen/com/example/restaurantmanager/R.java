30d9d60eeb5ecbd4411b70d7ffd0fc88ab7ceb83
Kseniia <kseniia.pelykh@gmail.com>
2014-08-31 00:33:59 +0200

    added activity for "primi", "secondi", "dolce", "antipasti"

--- gen/com/example/restaurantmanager/R.java
+++ gen/com/example/restaurantmanager/R.java
@@ -1193,12 +1193,24 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_antipasti=0x7f030018;
+        public static final int activity_dolce=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int activity_primi=0x7f03001b;
+        public static final int activity_secondi=0x7f03001c;
+        public static final int fragment_antipasti=0x7f03001d;
+        public static final int fragment_dolce=0x7f03001e;
+        public static final int fragment_main=0x7f03001f;
+        public static final int fragment_primi=0x7f030020;
+        public static final int fragment_secondi=0x7f030021;
+        public static final int support_simple_spinner_dropdown_item=0x7f030022;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int antipasti=0x7f0c0000;
+        public static final int dolce=0x7f0c0001;
+        public static final int main=0x7f0c0002;
+        public static final int primi=0x7f0c0003;
+        public static final int secondi=0x7f0c0004;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1245,9 +1257,14 @@ containing a value of this type.
         public static final int antipasti=0x7f0a0011;
         public static final int app_name=0x7f0a000d;
         public static final int dolce=0x7f0a0012;
+        public static final int hello_world=0x7f0a0015;
         public static final int menu=0x7f0a0013;
         public static final int primi=0x7f0a000f;
         public static final int secondi=0x7f0a0010;
+        public static final int title_activity_antipasti=0x7f0a0017;
+        public static final int title_activity_dolce=0x7f0a0018;
+        public static final int title_activity_primi=0x7f0a0014;
+        public static final int title_activity_secondi=0x7f0a0016;
     }
     public static final class style {
         /** 
--- src/com/example/restaurantmanager/Antipasti.java
+++ src/com/example/restaurantmanager/Antipasti.java
@@ -1,22 +1,22 @@
 package com.example.restaurantmanager;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class Antipasti extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_antipasti);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
@@ -28,7 +28,7 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.antipasti, menu);
 		return true;
 	}
 
@@ -55,34 +55,10 @@ public class MainActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+			View rootView = inflater.inflate(R.layout.fragment_antipasti,
+					container, false);
 			return rootView;
 		}
-	}
-	
-	// aprire la lista dei primi
-	public void openPrimi(View view) {
-	    Intent intent = new Intent(this, Primi.class);
-	    startActivity(intent);
-	}
-	
-	// aprire la lista dei secondi
-	public void openSecondi(View view) {
-	    Intent intent = new Intent(this, Secondi.class);
-	    startActivity(intent);
-	}
-	
-	// aprire la lista dei antipasti
-	public void openAntipasti(View view) {
-	    Intent intent = new Intent(this, Antipasti.class);
-	    startActivity(intent);
-	}
-	
-	// aprire la lista dei secondi
-	public void openDolce(View view) {
-	    Intent intent = new Intent(this, Dolce.class);
-	    startActivity(intent);
 	}
 
 }
